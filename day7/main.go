6e1b9edd2599844f053aea6cbd0f6ceca4e792e8
Brian Holley <brian.holley@hotmail.com>
2023-12-07 21:10:30 -0800

    Day 7, part 2

--- day7/main.go
+++ day7/main.go
@@ -14,7 +14,7 @@ func main() {
 	fmt.Println(result)
 }
 
-func cardPriority(card byte) int {
+func cardPriorityPart1(card byte) int {
 	order := []byte{'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'}
 	for i, v := range order {
 		if v == card {
@@ -24,15 +24,25 @@ func cardPriority(card byte) int {
 	panic("Unknown card")
 }
 
-func sortHand(hand string) string {
+func cardPriorityPart2(card byte) int {
+	order := []byte{'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'}
+	for i, v := range order {
+		if v == card {
+			return i
+		}
+	}
+	panic("Unknown card")
+}
+
+func sortHand(hand string, priority func(card byte) int) string {
 	cards := []byte(hand)
 	sort.Slice(cards, func(i, j int) bool {
-		return cardPriority(cards[i]) < cardPriority(cards[j])
+		return priority(cards[i]) < priority(cards[j])
 	})
 	return string(cards)
 }
 
-func scoreHand(hand string) int {
+func scoreHandPart1(hand string) int {
 	groups := []int{0, 0, 0, 0}
 	card := hand[0]
 	count := 1
@@ -81,6 +91,82 @@ func scoreHand(hand string) int {
 	return 7
 }
 
+func scoreHandPart2(hand string) int {
+	groups := []int{0, 0, 0, 0}
+	card := hand[0]
+	count := 1
+	jokers := 0
+	if hand[0] == 'J' {
+		jokers++
+	}
+	for i := 1; i < len(hand); i++ {
+		if hand[i] == 'J' {
+			jokers++
+			continue
+		}
+		if hand[i] == card {
+			count++
+		} else {
+			if count > 1 {
+				groups[count-2]++
+			}
+			card = hand[i]
+			count = 1
+		}
+	}
+	if count > 1 {
+		groups[count-2]++
+	}
+
+	fmt.Println(hand, groups, jokers)
+
+	for i := len(groups) - 1; i >= 0; i-- {
+		if groups[i] > 0 {
+			groups[i+jokers]++
+			groups[i]--
+			jokers = 0
+			break
+		}
+	}
+	if jokers == 5 {
+		groups[3]++
+		jokers = 0
+	}
+	if jokers > 0 {
+		groups[jokers-1]++
+		jokers = 0
+	}
+
+	fmt.Println(hand, groups, jokers)
+
+	// Five of a kind
+	if groups[3] > 0 {
+		return 1
+	}
+	// Four of a kind
+	if groups[2] > 0 {
+		return 2
+	}
+	// Full house
+	if groups[1] > 0 && groups[0] > 0 {
+		return 3
+	}
+	// Three of a kind
+	if groups[1] > 0 {
+		return 4
+	}
+	// Two pair
+	if groups[0] > 1 {
+		return 5
+	}
+	// One pair
+	if groups[0] > 0 {
+		return 6
+	}
+	// High card
+	return 7
+}
+
 type player struct {
 	hand   string
 	sorted string
@@ -88,7 +174,7 @@ type player struct {
 	score  int
 }
 
-func sortPlayers(players []player) {
+func sortPlayers(players []player, priority func(card byte) int) {
 	sort.Slice(players, func(a, b int) bool {
 		if players[a].score < players[b].score {
 			return true
@@ -97,8 +183,8 @@ func sortPlayers(players []player) {
 		}
 
 		for i := 0; i < len(players[a].hand) && i < len(players[b].hand); i++ {
-			aPri := cardPriority(players[a].hand[i])
-			bPri := cardPriority(players[b].hand[i])
+			aPri := priority(players[a].hand[i])
+			bPri := priority(players[b].hand[i])
 			if aPri < bPri {
 				return true
 			} else if bPri < aPri {
@@ -116,12 +202,12 @@ func part1(stdin *bufio.Scanner) string {
 		hand := line[0]
 		bid, _ := strconv.Atoi(line[1])
 
-		sorted := sortHand(hand)
-		score := scoreHand(sorted)
+		sorted := sortHand(hand, cardPriorityPart1)
+		score := scoreHandPart1(sorted)
 		players = append(players, player{hand, sorted, bid, score})
 	}
 
-	sortPlayers(players)
+	sortPlayers(players, cardPriorityPart1)
 
 	score := 0
 	for i, p := range players {
@@ -132,5 +218,23 @@ func part1(stdin *bufio.Scanner) string {
 }
 
 func part2(stdin *bufio.Scanner) string {
-	return "part2"
+	players := []player{}
+	for stdin.Scan() {
+		line := strings.Split(stdin.Text(), " ")
+		hand := line[0]
+		bid, _ := strconv.Atoi(line[1])
+
+		sorted := sortHand(hand, cardPriorityPart2)
+		score := scoreHandPart2(sorted)
+		players = append(players, player{hand, sorted, bid, score})
+	}
+
+	sortPlayers(players, cardPriorityPart2)
+
+	score := 0
+	for i, p := range players {
+		fmt.Println(p.hand, p.sorted, p.bid, p.score)
+		score += (len(players) - i) * p.bid
+	}
+	return fmt.Sprint(score)
 }
