06dcd3db7defdca6896a49b4fdf65a72466f5afe
Brian Holley <brian.holley@hotmail.com>
2023-12-08 22:57:46 -0800

    Day 9, part 2

--- day9/main.go
+++ day9/main.go
@@ -11,31 +11,35 @@ func main() {
 	fmt.Println(result)
 }
 
+func calcDeltasForHistory(history []int) [][]int {
+	deltas := [][]int{history}
+	for {
+		latest := deltas[len(deltas)-1]
+		current := []int{}
+		nonzero := false
+		for i := 0; i < len(latest)-1; i++ {
+			d := latest[i+1] - latest[i]
+			current = append(current, d)
+			if d != 0 {
+				nonzero = true
+			}
+		}
+
+		// fmt.Println(current)
+		deltas = append(deltas, current)
+		if !nonzero {
+			break
+		}
+	}
+	return deltas
+}
+
 func part1(stdin *bufio.Scanner) string {
 	sum := 0
 	for stdin.Scan() {
 		history := lib.ParseStringOfIntsSpaceDelimited(stdin.Text())
-		// fmt.Println(history)
 
-		deltas := [][]int{history}
-		for {
-			latest := deltas[len(deltas)-1]
-			current := []int{}
-			nonzero := false
-			for i := 0; i < len(latest)-1; i++ {
-				d := latest[i+1] - latest[i]
-				current = append(current, d)
-				if d != 0 {
-					nonzero = true
-				}
-			}
-
-			// fmt.Println(current)
-			deltas = append(deltas, current)
-			if !nonzero {
-				break
-			}
-		}
+		deltas := calcDeltasForHistory(history)
 
 		next := 0
 		for level := len(deltas) - 2; level >= 0; level-- {
@@ -49,5 +53,22 @@ func part1(stdin *bufio.Scanner) string {
 }
 
 func part2(stdin *bufio.Scanner) string {
-	return "part2"
+	sum := 0
+	for stdin.Scan() {
+		history := lib.ParseStringOfIntsSpaceDelimited(stdin.Text())
+
+		deltas := calcDeltasForHistory(history)
+
+		// prev + [level+1] = [level]
+		prev := 0
+		for level := len(deltas) - 2; level >= 0; level-- {
+			prev = deltas[level][0] - prev
+			// fmt.Println(prev, deltas[level])
+		}
+
+		// fmt.Println(next)
+		sum += prev
+		// fmt.Println()
+	}
+	return fmt.Sprint(sum)
 }
