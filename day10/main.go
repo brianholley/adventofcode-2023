cd38c5a976e620bd34d1d589d1ea9776d4ce4231
Brian Holley <brian.holley@hotmail.com>
2023-12-10 23:10:34 -0800

    Day 10, part 2

--- day10/main.go
+++ day10/main.go
@@ -13,8 +13,8 @@ func main() {
 
 const NORTH int = 1
 const EAST int = 2
-const WEST int = 4
-const SOUTH int = 8
+const SOUTH int = 4
+const WEST int = 8
 
 type node struct {
 	row, col int
@@ -49,7 +49,7 @@ func walk(rstart int, cstart int, pipes [][]int, dist [][]int) {
 	}
 }
 
-func part1(stdin *bufio.Scanner) string {
+func parsePipes(stdin *bufio.Scanner) ([][]int, int, int) {
 	pipes := [][]int{}
 
 	rstart, cstart := 0, 0
@@ -61,46 +61,29 @@ func part1(stdin *bufio.Scanner) string {
 			switch v {
 			case '|':
 				row = append(row, NORTH|SOUTH)
-				break
 			case '-':
 				row = append(row, EAST|WEST)
-				break
 			case 'L':
 				row = append(row, NORTH|EAST)
-				break
 			case 'J':
 				row = append(row, NORTH|WEST)
-				break
 			case '7':
 				row = append(row, SOUTH|WEST)
-				break
 			case 'F':
 				row = append(row, SOUTH|EAST)
-				break
 			case '.':
 				row = append(row, 0)
-				break
 			case 'S':
 				row = append(row, 0)
 				rstart, cstart = len(pipes), i
-				break
 			default:
-				panic("Unkonwn pipe")
+				fmt.Println(string(v))
+				panic("Unknown pipe")
 			}
 		}
 		pipes = append(pipes, row)
 	}
 
-	dist := [][]int{}
-	for i := 0; i < len(pipes); i++ {
-		dist = append(dist, make([]int, len(pipes[i])))
-		for j := 0; j < len(pipes[i]); j++ {
-			dist[i][j] = -1
-		}
-	}
-	dist[rstart][cstart] = 0
-
-	// connect start
 	if rstart > 0 && pipes[rstart-1][cstart]&SOUTH > 0 {
 		pipes[rstart][cstart] |= NORTH
 	}
@@ -113,12 +96,52 @@ func part1(stdin *bufio.Scanner) string {
 	if cstart < len(pipes[rstart])-1 && pipes[rstart][cstart+1]&WEST > 0 {
 		pipes[rstart][cstart] |= EAST
 	}
+	return pipes, rstart, cstart
+}
+
+func printPipes(pipes [][]int) {
+	for r := range pipes {
+		for c := range pipes[r] {
+			switch pipes[r][c] {
+			case NORTH | SOUTH:
+				fmt.Print("|")
+			case EAST | WEST:
+				fmt.Print("-")
+			case NORTH | EAST:
+				fmt.Print("L")
+			case NORTH | WEST:
+				fmt.Print("J")
+			case SOUTH | WEST:
+				fmt.Print("7")
+			case SOUTH | EAST:
+				fmt.Print("F")
+			case 0:
+				fmt.Print(".")
+			case -1:
+				fmt.Print(".")
+			case -2:
+				fmt.Print("I")
+			default:
+				fmt.Print("?")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func part1(stdin *bufio.Scanner) string {
+	pipes, rstart, cstart := parsePipes(stdin)
+
+	dist := [][]int{}
+	for i := 0; i < len(pipes); i++ {
+		dist = append(dist, make([]int, len(pipes[i])))
+		for j := 0; j < len(pipes[i]); j++ {
+			dist[i][j] = -1
+		}
+	}
+	dist[rstart][cstart] = 0
 
 	walk(rstart, cstart, pipes, dist)
-
-	// for _, r := range pipes {
-	// 	fmt.Println(r)
-	// }
 
 	max := 0
 	for _, r := range dist {
@@ -129,6 +152,100 @@ func part1(stdin *bufio.Scanner) string {
 	return fmt.Sprint(max)
 }
 
+func markMainLoop(pipes [][]int, dist [][]int) [][]int {
+
+	loop := [][]int{}
+	for i := 0; i < len(pipes); i++ {
+		loop = append(loop, make([]int, len(pipes[i])))
+		for j := 0; j < len(pipes[i]); j++ {
+			loop[i][j] = 0
+		}
+	}
+
+	// rstart, cstart := 0, 0
+	rend, cend := 0, 0
+	max := 0
+	for r, _ := range dist {
+		for c, _ := range dist[r] {
+			// if dist[r][c] == 0 {
+			// 	rstart, cstart = r, c
+			// }
+			if dist[r][c] > max {
+				rend, cend = r, c
+				max = dist[r][c]
+			}
+		}
+	}
+
+	adj := []node{{rend, cend}}
+
+	for len(adj) > 0 {
+		n := adj[0]
+		adj = adj[1:]
+		loop[n.row][n.col] = pipes[n.row][n.col]
+		if n.row > 0 && pipes[n.row][n.col]&NORTH > 0 && dist[n.row-1][n.col] < dist[n.row][n.col] && dist[n.row-1][n.col] >= 0 {
+			adj = append(adj, node{n.row - 1, n.col})
+		}
+		if n.row < len(pipes)-1 && pipes[n.row][n.col]&SOUTH > 0 && dist[n.row+1][n.col] < dist[n.row][n.col] && dist[n.row+1][n.col] >= 0 {
+			adj = append(adj, node{n.row + 1, n.col})
+		}
+		if n.col > 0 && pipes[n.row][n.col]&WEST > 0 && dist[n.row][n.col-1] < dist[n.row][n.col] && dist[n.row][n.col-1] >= 0 {
+			adj = append(adj, node{n.row, n.col - 1})
+		}
+		if n.col < len(pipes[n.row])-1 && pipes[n.row][n.col]&EAST > 0 && dist[n.row][n.col+1] < dist[n.row][n.col] && dist[n.row][n.col+1] >= 0 {
+			adj = append(adj, node{n.row, n.col + 1})
+		}
+	}
+
+	return loop
+}
+
 func part2(stdin *bufio.Scanner) string {
-	return "part2"
+	pipes, rstart, cstart := parsePipes(stdin)
+
+	dist := [][]int{}
+	for i := 0; i < len(pipes); i++ {
+		dist = append(dist, make([]int, len(pipes[i])))
+		for j := 0; j < len(pipes[i]); j++ {
+			dist[i][j] = -1
+		}
+	}
+	dist[rstart][cstart] = 0
+
+	walk(rstart, cstart, pipes, dist)
+
+	loop := markMainLoop(pipes, dist)
+
+	inside := 0
+	for r, _ := range loop {
+		in := false
+		need := 0
+		for c, _ := range loop[r] {
+			if loop[r][c]&(NORTH|SOUTH) == (NORTH | SOUTH) {
+				in = !in
+			} else if loop[r][c]&(EAST|WEST) == (EAST | WEST) {
+				// no op
+			} else if loop[r][c] > 0 {
+				if need == 0 {
+					if loop[r][c]&NORTH > 0 {
+						need = SOUTH
+					} else {
+						need = NORTH
+					}
+				} else if need > 0 && loop[r][c]&need > 0 {
+					in = !in
+					need = 0
+				} else {
+					need = 0
+				}
+			} else if in {
+				inside++
+				loop[r][c] = -2
+			}
+		}
+	}
+
+	// printPipes(loop)
+
+	return fmt.Sprint(inside)
 }
